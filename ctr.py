ba137041a748adf00c23e59c444d239863ccf88a
Neriya Mazzuz <neriya92@gmail.com>
2022-01-02 20:51:26 +0200

    Add Precision-Recall Graph

--- ctr.py
+++ ctr.py
@@ -1,20 +1,21 @@
-# Adi Paz
-# Neriya Mazzuz
-# Gil Shamay 
-seed = 415
-
 import time
 import pandas as pd
+import matplotlib.pyplot as plt
 import zipfile
 from io import StringIO
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import roc_auc_score
 from sklearn.ensemble import RandomForestClassifier
-import category_encoders as ce
+from sklearn.metrics import precision_recall_curve, PrecisionRecallDisplay, f1_score
 from scipy import stats
 import lightgbm as lgb
 import numpy as np
 from sklearn import preprocessing
+
+# Adi Paz
+# Neriya Mazzuz
+# Gil Shamay
+seed = 415
 
 #  LightGBM vs  RandomForest for CTR
 ####################################################################################
@@ -49,7 +50,7 @@ from sklearn import preprocessing
 ###############################################################################################
 
 
-submit = False # test on test dat as uploaded to https://www.kaggle.com/c/bgu-rs/data See test_file_3/test_file.csv
+submit = False  # test on test data as uploaded to https://www.kaggle.com/c/bgu-rs/data See test_file_3/test_file.csv
 maxFiles = 10
 pd.options.display.width = 0
 
@@ -64,47 +65,40 @@ def readCSVFromZip(archive, file):
     return df
 
 
-def readTrainData():
+def readTrainData(encode):
     beginTime = time.time()
     archive = zipfile.ZipFile('./data/bgu-rs.zip', 'r')
-    i=0
+    i = 0
     for file in archive.filelist:
         if "part-" in file.filename and ".csv" in file.filename and i < maxFiles:
             fileBeginTime = time.time()
-            if i==0:
+            if i == 0:
                 df = readCSVFromZip(archive, file)
             else:
                 new_df = readCSVFromZip(archive, file)
                 df = pd.concat([df, new_df])
 
-            print("file: " + str(i) + " handle time[" + str(time.time() - fileBeginTime)+ "]")
-            i = i+1
+            print("file: " + str(i) + " handle time[" + str(time.time() - fileBeginTime) + "]")
+            i = i + 1
 
     trainDf, testDf = train_test_split(df, train_size=0.99, random_state=seed)
     trainDf = trainDf.dropna()  # todo: do we need this?
     testDf = testDf.dropna()
     trainY = trainDf.pop('is_click')
     trainX = trainDf
-    trainX, ce_target_encoder = preprocessData(trainX, trainY)
+    trainX = preprocessData(trainX, encode)
     print(trainX.describe())
 
     testY = testDf.pop('is_click')
     testX = testDf
-    testX, ce_target_encoder = preprocessData(testX, ce_target_encoder=ce_target_encoder)
+    testX = preprocessData(testX, encode)
     print(testX.describe())
     print("Number of rows in train: " + str(trainX.shape[0]))
     print("Time taken: [" + str(time.time() - beginTime) + "]")
-    return trainX, testX, trainY, testY, ce_target_encoder
+    return trainX, testX, trainY, testY
 
 
-def target_encode(X, y, categorical_columns):
-    ce_target_encoder = ce.TargetEncoder(cols=categorical_columns, min_samples_leaf=10)
-    ce_target_encoder.fit(X, y)
-    X = ce_target_encoder.transform(X)
-    return X, ce_target_encoder
-
-
-def preprocessData(df, y=None, ce_target_encoder=None):
+def preprocessData(df, encode):
     fitBeginTime = time.time()
     df['os_is_six'] = [1 if x == 6 else 0 for x in df['os_family']]
     categorical_columns = ['user_id_hash', 'target_id_hash', 'syndicator_id_hash', 'campaign_id_hash',
@@ -114,18 +108,11 @@ def preprocessData(df, y=None, ce_target_encoder=None):
     for column_name in categorical_columns:
         df[column_name] = pd.Categorical(df[column_name])
 
-    """""
-    if ce_target_encoder==None:
-        df, ce_target_encoder = target_encode(df, y, categorical_columns)
-    else:
-        df = ce_target_encoder.transform(df)
-    """
-
     print("transformDataToX_Y took[" + str(time.time() - fitBeginTime) + "]")
 
     df.pop('page_view_start_time')
 
-    df['night'] = [1 if x<=6 else 0 for x in df['time_of_day']]
+    df['night'] = [1 if x <= 6 else 0 for x in df['time_of_day']]
     df['evening'] = [1 if x >= 17 else 0 for x in df['time_of_day']]
 
     # time + gmt_offset --> new time ; remove GMT // (save time.. ?)
@@ -149,33 +136,33 @@ def preprocessData(df, y=None, ce_target_encoder=None):
     # df = pd.concat([df, df['target_item_taxonomy'].str.get_dummies('~').add_prefix('C_')], axis=1)
     # TODO: We should  find how to use this data
     df.pop('target_item_taxonomy')
-    #df.pop('user_id_hash')
-    #df.pop('target_id_hash')
-    #df.pop('syndicator_id_hash')
-    #df.pop('campaign_id_hash')
-    #df.pop('placement_id_hash')
-    #df.pop('publisher_id_hash')
-    #df.pop('source_id_hash')
-    # return df, ce_target_encoder
+    # df.pop('user_id_hash')
+    # df.pop('target_id_hash')
+    # df.pop('syndicator_id_hash')
+    # df.pop('campaign_id_hash')
+    # df.pop('placement_id_hash')
+    # df.pop('publisher_id_hash')
+    # df.pop('source_id_hash')
 
-    #this is needed for random forests ! RandomForestClassifier - it kills the auc for the lgbm
-    df = df.apply(preprocessing.LabelEncoder().fit_transform)
-    return df, None
-
-
+    # this is needed for random forests ! RandomForestClassifier - it kills the auc for the lgbm
+    if encode:
+        return df.apply(preprocessing.LabelEncoder().fit_transform)
+    return df
 
 
-def builedModelRF(trainX, trainY):
-    RF =  RandomForestClassifier(verbose=2, n_estimators=100, max_depth=20, min_samples_split=5, n_jobs=4)
-    RF.fit(trainX,trainY)
-    return RF
+def buildModelRF():
+    trainX, testX, trainY, testY = readTrainData(True)
+    RF = RandomForestClassifier(verbose=2, n_estimators=100, max_depth=20, min_samples_split=5, n_jobs=4)
+    RF.fit(trainX, trainY)
+    return RF, testX, testY
 
 
-def builedModelLightGBM(trainX, trainY):
+def buildModelLightGBM():
+    trainX, testX, trainY, testY = readTrainData(False)
     categorical_features = ['user_id_hash', 'target_id_hash', 'syndicator_id_hash', 'campaign_id_hash',
-                           'placement_id_hash', 'publisher_id_hash', 'source_id_hash',
-                           'source_item_type', 'browser_platform', 'country_code', 'region',
-                           'os_family', 'day_of_week']
+                            'placement_id_hash', 'publisher_id_hash', 'source_id_hash',
+                            'source_item_type', 'browser_platform', 'country_code', 'region',
+                            'os_family', 'day_of_week']
     taxonomy_categories = ['LIFE', 'BUSINESS', 'ENTERTAINMENT', 'HEALTH', 'TECH', 'AUTOS', 'FOOD', 'SPORTS', 'MUSIC',
                            'PETS', 'NEWS', 'FASHION', 'FOOTBALL']
     categorical_features = categorical_features + taxonomy_categories
@@ -200,7 +187,7 @@ def builedModelLightGBM(trainX, trainY):
     fitBeginTime = time.time()
     model = lgb.train(parameters, train_data, num_boost_round=200)
     print("Training model took[" + str(time.time() - fitBeginTime) + "]")
-    return model
+    return model, testX, testY
 
 
 def trainModel(X, y, model):
@@ -227,9 +214,11 @@ def evaluateModel(X, y, model):
     normRes = np.vectorize(normalizeResults)(testRes)
     AUCNorm = roc_auc_score(y, normRes)
 
+    precision, recall, _ = precision_recall_curve(y, testRes)
+
     # todo: Check that the res data is not <0 or >1 and fix if it does
     print('test: AUC[' + str(AUC) + ']' + 'test: AUCNorm[' + str(AUCNorm) + ']' + str(stats.describe(testRes)))
-    return AUC
+    return AUC, precision, recall
 
 
 def loadUncompressed(path):
@@ -251,20 +240,26 @@ def loadUncompressed(path):
 def run():
     # read the data and fit
     print("-------------------------------")
-    trainX, testX, trainY, testY, ce_target_encoder = readTrainData()
 
     modelLRF = None
     modelLGBM = None
-    modelLRF =builedModelRF(trainX, trainY)
-    modelLGBM = builedModelLightGBM(trainX, trainY)
+    modelLRF, RFtestX, RFtestY = buildModelRF()
+    modelLGBM, LGBMtestX, LGBMtestY = buildModelLightGBM()
 
     if modelLRF is not None:
-        aucRF = evaluateModel(testX, testY, modelLRF)
+        aucRF, precisionRF, recallRF = evaluateModel(RFtestX, RFtestY, modelLRF)
+        dispRF = PrecisionRecallDisplay(precisionRF, recallRF)
+        dispRF.plot()
+        #plt.show()
         print('AUC RF[' + str(aucRF) + ']')
 
     if modelLGBM is not None:
-        aucLGBM = evaluateModel(testX, testY, modelLGBM)
+        aucLGBM, precisionLGBM, recallLGBM = evaluateModel(LGBMtestX, LGBMtestY, modelLGBM)
+        dispLGBM = PrecisionRecallDisplay(precisionLGBM, recallLGBM)
+        dispLGBM.plot()
+        plt.show()
         print('AUC LGBM[' + str(aucLGBM) + ']')
+
 
 run()
 print(" ---- Done ---- ")
